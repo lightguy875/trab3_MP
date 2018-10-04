862f1f543ba9b1f3f43d542d26ba589f828d89da
lightguy875 <luz875@gmail.com>
2018-10-04 19:43:04 -0300

    first commit

---
+++ Source/Sample/program.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * @brief Simple hello world program
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int main(int argc, char const *argv[])
+{
+    //print function 
+    cout << "Hello, World!"; 
+    
+    return 0;
+}
+
